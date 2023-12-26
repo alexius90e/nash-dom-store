c854454565260aff09dc162b2dbdaa150b426fbf
Aliaksei Karzhou <alexius90e@gmail.com>
2023-12-26 17:12:32 +0300

    fix: fixed dual slider

--- js/double-range-slider.js
+++ js/double-range-slider.js
@@ -1,14 +1,14 @@
-const doubleRangeSliders = document.querySelectorAll('.double-range-slider');
+const doubleRangeSliders = document.querySelectorAll(".double-range-slider");
 
 doubleRangeSliders.forEach((doubleRangeSlider) => {
-  const rangeWrapper = doubleRangeSlider.querySelector('.double-range-slider__inputs');
-  const lowerSlider = doubleRangeSlider.querySelector('.double-range-slider__input_lower');
-  const upperSlider = doubleRangeSlider.querySelector('.double-range-slider__input_upper');
-  const lowerLabel = doubleRangeSlider.querySelector('.double-range-slider__label_lower');
-  const upperLabel = doubleRangeSlider.querySelector('.double-range-slider__label_upper');
-  const maxValue = parseInt(upperSlider.getAttribute('max'));
-  const progress = document.createElement('div');
-  progress.classList.add('double-range-slider__inputs-progress');
+  const rangeWrapper = doubleRangeSlider.querySelector(".double-range-slider__inputs");
+  const lowerSlider = doubleRangeSlider.querySelector(".double-range-slider__input_lower");
+  const upperSlider = doubleRangeSlider.querySelector(".double-range-slider__input_upper");
+  const lowerLabel = doubleRangeSlider.querySelector(".double-range-slider__label_lower");
+  const upperLabel = doubleRangeSlider.querySelector(".double-range-slider__label_upper");
+  const maxValue = parseInt(upperSlider.getAttribute("max"));
+  const progress = document.createElement("div");
+  progress.classList.add("double-range-slider__inputs-progress");
   rangeWrapper.append(progress);
 
   let lowerVal = parseInt(lowerSlider.value);
@@ -17,23 +17,20 @@ doubleRangeSliders.forEach((doubleRangeSlider) => {
   function updateValue() {
     lowerVal = parseInt(lowerSlider.value);
     upperVal = parseInt(upperSlider.value);
-    lowerLabel.value = lowerVal * maxValue;
-    upperLabel.value = upperVal * maxValue;
+    lowerLabel.value = lowerVal;
+    upperLabel.value = upperVal;
     updateProgress();
   }
 
   function updateProgress() {
     const maxPersentage = 96.5;
-    const thumbWidth = 22;
-    progress.style.left = `calc(${(lowerVal / maxValue) * maxPersentage}% + ${thumbWidth}px)`;
-    progress.style.right = `calc(${
-      ((maxValue - upperVal) / maxValue) * maxPersentage
-    }% + ${thumbWidth}px)`;
+    progress.style.left = `calc(${(lowerVal / maxValue) * maxPersentage}%)`;
+    progress.style.right = `calc(${((maxValue - upperVal) / maxValue) * maxPersentage}% )`;
   }
 
   updateProgress();
 
-  upperSlider.addEventListener('input', () => {
+  upperSlider.addEventListener("input", () => {
     updateValue();
     if (upperVal < lowerVal + 1) {
       lowerSlider.value = upperVal - 1;
@@ -41,7 +38,7 @@ doubleRangeSliders.forEach((doubleRangeSlider) => {
     }
   });
 
-  lowerSlider.addEventListener('input', () => {
+  lowerSlider.addEventListener("input", () => {
     updateValue();
     if (lowerVal > upperVal - 1) {
       upperSlider.value = lowerVal + 1;
@@ -49,16 +46,17 @@ doubleRangeSliders.forEach((doubleRangeSlider) => {
     }
   });
 
-  lowerLabel.addEventListener('change', (e) => {
+  lowerLabel.addEventListener("change", (e) => {
     const newValue = parseInt(e.target.value);
     if (newValue <= upperSlider.value) {
       lowerSlider.value = newValue;
     } else {
       lowerLabel.value = lowerSlider.value;
     }
+    updateValue();
   });
 
-  upperLabel.addEventListener('change', (e) => {
+  upperLabel.addEventListener("change", (e) => {
     const newValue = parseInt(e.target.value);
 
     if (newValue >= lowerSlider.value) {
@@ -67,5 +65,6 @@ doubleRangeSliders.forEach((doubleRangeSlider) => {
     } else {
       upperLabel.value = upperSlider.value;
     }
+    updateValue();
   });
 });
